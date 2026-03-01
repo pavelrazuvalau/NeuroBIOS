0738f7e813c280be39af39e905c4dfbcba94cd2e
Pavel Razuvalau <pavel@razuvalov.com>
2026-03-01 12:53:14 +0100

    feat: implement fsm linear transition

--- src/fsm.py
+++ src/fsm.py
@@ -1,14 +1,23 @@
 class StateMachine:
-    ALLOWED_STATES = ("IDLE", "WORKING", "END")
 
-    def __init__(self):
-        self.state = self.ALLOWED_STATES[0]
+    def __init__(self, allowed_states):
+        self.allowed_states = allowed_states
+        self.state = self.allowed_states[0]
 
     def get_state(self):
         print(f"Current State: {self.state}")
 
-    def set_state(self, state):
-        if state in self.ALLOWED_STATES:
-            self.state = state
+    def set_state(self, new_state):
+        is_new_state_valid = new_state in self.allowed_states
+        is_transition_valid = is_new_state_valid and self.__is_transition_permitted(new_state)
+
+        if is_transition_valid:
+            self.state = new_state
         else:
-            print(f"Wrong stage: expected one in the list: {self.ALLOWED_STATES}")
+            print(f"Failed to set new state {new_state}: expected one in the list after {self.state}: {self.allowed_states}")
+
+    def __is_transition_permitted(self, new_state):
+        current_index = self.allowed_states.index(self.state)
+        new_index = self.allowed_states.index(new_state)
+
+        return new_index - current_index == 1
--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from fsm import StateMachine
 
 
 def main():
-    fsm = StateMachine()
+    fsm = StateMachine(("IDLE", "WORKING", "VERIFYING", "END"))
     fsm.get_state()
     fsm.set_state("WORKING")
     fsm.get_state()
     fsm.set_state("FLYING_PIG")
+    fsm.get_state()
+    fsm.set_state("END")
     fsm.get_state()
  
 if __name__ == "__main__":
