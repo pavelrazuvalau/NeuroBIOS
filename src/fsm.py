f643c3ec5e4792bc5a9d61aa5526fafc924d424d
Pavel Razuvalau <pavel@razuvalov.com>
2026-03-03 10:53:39 +0100

    refactor: address minor pythonic improvements

--- src/fsm.py
+++ src/fsm.py
@@ -6,35 +6,35 @@ class StateMachine:
 
     def __init__(self, flow_actions, system_actions = None):
         self.state = self.IDLE_STATE
+        self.context = {}
         self.is_flow_running = False
 
-        self.__flow_states = tuple()
+        self._flow_states = tuple()
 
-        self.__system_actions = system_actions or {}
-        self.__flow_actions = flow_actions
-        self.__actions = self.__system_actions | self.__flow_actions
+        self._system_actions = system_actions or {}
+        self._flow_actions = flow_actions
+        self._actions = self._system_actions | self._flow_actions
 
-        self.__is_unhandled_failure_occured = False
-        self.__next_state_override = None
+        self._is_unhandled_failure_occurred = False
+        self._next_state_override = None
 
     def set_flow(self, flow_states, context = None):
         if not flow_states:
-            print("Error: flow cannot be empty")
-            return
+            raise ValueError("Error: Flow cannot be empty")
 
-        self.context = context if context is not None else {}
         self.state = flow_states[0]
+        self.context = context if context is not None else {}
         self.is_flow_running = True
 
-        self.__flow_states = flow_states
+        self._flow_states = flow_states
 
     def get_state(self):
         return { "state": self.state, "context": self.context }
 
     def go_to_next_state(self):
-        if self.__next_state_override is not None:
-            self.__set_state(self.__next_state_override, True)
-            self.__next_state_override = None
+        if self._next_state_override is not None:
+            self._set_state(self._next_state_override, True)
+            self._next_state_override = None
             self.is_flow_running = True
             return
 
@@ -42,60 +42,60 @@ class StateMachine:
             print(f"The flow is stopped at {self.state} state")
             return
         
-        if self.__is_unhandled_failure_occured:
-            self.__set_state(self.ESCALATE_STATE)
-            self.__is_unhandled_failure_occured = False
+        if self._is_unhandled_failure_occurred:
+            self._set_state(self.ESCALATE_STATE)
+            self._is_unhandled_failure_occurred = False
             return
         
-        if self.__has_reached_stop_state():
+        if self._has_reached_stop_state():
             self.is_flow_running = False
             return
         
-        next_state_index = self.__get_state_index(self.state) + 1
-        is_next_step_available = next_state_index < len(self.__flow_states)
-        next_state = self.__flow_states[next_state_index] if is_next_step_available else self.END_STATE
+        next_state_index = self._get_state_index(self.state) + 1
+        is_next_step_available = next_state_index < len(self._flow_states)
+        next_state = self._flow_states[next_state_index] if is_next_step_available else self.END_STATE
 
-        self.__set_state(next_state)
+        self._set_state(next_state)
 
     def execute_state(self):
-        current_action = self.__actions.get(self.state)
+        current_action = self._actions.get(self.state)
 
         if current_action:
-            response = current_action(self.context)
+            response = current_action(self.context) or {}
 
             context_update = response.get("context_update", {})
             self.context |= context_update
 
-            self.__next_state_override = response.get("next_state_override", None)
+            self._next_state_override = response.get("next_state_override", None)
 
             is_success = response.get("success", True)
-            self.__is_unhandled_failure_occured = not is_success
+            self._is_unhandled_failure_occurred = not is_success
 
             return response.get("result")
         else:
             print(f"No action found for state {self.state}")
             return None
         
-    def __set_state(self, state, is_override = False):
-        if self.__is_transition_permitted(state, is_override):
+    def _set_state(self, state, is_override = False):
+        if self._is_transition_permitted(state, is_override):
             self.state = state
         else:
-            print(f"Failed to set new state {state}: expected one in the tuple after {self.state}: {self.__flow_states}")
+            print(f"Failed to set new state {state}: expected one in the tuple after {self.state}: {self._flow_states}")
 
-    def __is_transition_permitted(self, state, is_override):
+    def _is_transition_permitted(self, state, is_override):
         if is_override or state in self.STOP_STATES:
             return True
         
-        if state not in self.__flow_states:
+        if state not in self._flow_states:
             return False
 
-        current_index = self.__get_state_index(self.state)
-        new_index = self.__get_state_index(state)
+        current_index = self._get_state_index(self.state)
+        new_index = self._get_state_index(state)
 
         return new_index - current_index == 1
     
-    def __get_state_index(self, state):
-        return self.__flow_states.index(state)
+    def _get_state_index(self, state):
+        return self._flow_states.index(state)
         
-    def __has_reached_stop_state(self):
+    def _has_reached_stop_state(self):
         return self.state in self.STOP_STATES
