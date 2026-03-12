11b75f0740f14e7416b415544a0f5b58773b9e29
Pavel Razuvalau <pavel_razuvalau@epam.com>
2026-03-12 19:51:39 +0100

    feat: improve agent loop by calling next step plan after tool call

--- src/agents/coffee/prompts/task/coffee_plan_next_step_prompt.py
+++ src/agents/coffee/prompts/task/coffee_plan_next_step_prompt.py
@@ -1,3 +1,9 @@
 plan_step_prompt = """
-    Plan the next step that strictly follows the initial plan order based on the conversation history. Do no skip any steps from the initial plan.
+    Plan the next step based on the conversation history.
+
+    Rules:
+      - Describe each step you make
+      - When the history contains a tool call that returned a result, verify whether it's successful.
+      - When the recent tool call has failed, run it again.
+      - Call a tool that strictly follows the order of the plan. Do no skip any steps from the initial plan.
 """
